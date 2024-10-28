127d83e825ae9eaa3f8ddcbc68a3d3b2252836e0
evinyanitoz <eyanitoz@gmail.com>
2024-10-28 23:45:20 +0300

    Add project files.

---
+++ DICTIONARY/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DICTIONARY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> dic = new Dictionary<string,string>();
+            dic.Add("BOOK", "kitap");
+            dic.Add("glass", "bardak");
+
+            Console.WriteLine(dic["BOOK"]); // KARŞILIĞI NE İSE SÖZLÜK ANLAMI NE İSE O DÖNER YANİ ÇIKTIDA KİTAP DÖNECEK.
+
+            foreach (var key in dic)
+            {
+                Console.WriteLine(key.Key); //book,glass
+                Console.WriteLine(key.Value); //KİTAP BARDAK
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DICTIONARY/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DICTIONARY")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DICTIONARY")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2621b93a-72fb-48b1-9e62-1116dd5a0ff1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
